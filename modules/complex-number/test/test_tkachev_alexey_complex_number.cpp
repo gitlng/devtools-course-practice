5aa1fe2aa79aa5667ea53306ddbb2a9a67e87573
user <user@DESKTOP-FQ84EBM>
2021-03-27 14:46:04 +0300

    Added new tests, removed the previous ones

--- modules/complex-number/test/test_tkachev_alexey_complex_number.cpp
+++ modules/complex-number/test/test_tkachev_alexey_complex_number.cpp
@@ -10,96 +10,86 @@ TEST(Tkachev_Alexey_ComplexNumberTest, full_of_zeros_no_throw) {
     ASSERT_NO_THROW(ComplexNumber(real, imaginary));
 }
 
-TEST(Tkachev_Alexey_ComplexNumberTest, imaginary_only_no_throw) {
-    const double real = 0.0;
-    const double imaginary = 1.0;
-    ASSERT_NO_THROW(ComplexNumber(real, imaginary));
-}
+TEST(Tkachev_Alexey_ComplexNumberTest, distributive_law) {
+    const double real_1 = 10.2;
+    const double imaginary_1 = -30.5;
 
-TEST(Tkachev_Alexey_ComplexNumberTest, real_only_no_throw) {
-    const double real = 1.0;
-    const double imaginary = 0.0;
-    ASSERT_NO_THROW(ComplexNumber(real, imaginary));
-}
+    const double real_2 = 40.5;
+    const double imaginary_2 = 1.2;
 
-TEST(Tkachev_Alexey_ComplexNumberTest, summing) {
-    const double real_1 = -30.0;
-    const double imaginary_1 = 10.5;
-
-    const double real_2 = -20.4;
-    const double imaginary_2 = 20.9;
+    const double real_3 = -0.5;
+    const double imaginary_3 = 21.5;
 
     const ComplexNumber complex_number_1(real_1, imaginary_1);
     const ComplexNumber complex_number_2(real_2, imaginary_2);
+    const ComplexNumber complex_number_3(real_3, imaginary_3);
 
-    const double real_true_solution = -50.4;
-    const double imaginary_true_solution = 31.4;
+    const ComplexNumber first_expression =
+        complex_number_1 * (complex_number_2 + complex_number_3);
 
-    const ComplexNumber complex_numbers_true_sum(
-           real_true_solution, imaginary_true_solution);
+    const ComplexNumber second_expression =
+        complex_number_1 * complex_number_2 +
+        complex_number_1 * complex_number_3;
 
-    ASSERT_EQ(complex_number_1 + complex_number_2,
-        complex_numbers_true_sum);
+    ASSERT_EQ(first_expression, second_expression);
 }
 
-TEST(Tkachev_Alexey_ComplexNumberTest, subtraction) {
-    const double real_1 = 10.0;
-    const double imaginary_1 = 25.4;
+TEST(Tkachev_Alexey_ComplexNumberTest, sum_with_im_zero) {
+    const double real_1 = 5.0;
+    const double imaginary_1 = 0.0;
 
-    const double real_2 = -150.5;
+    const double real_2 = 2.0;
     const double imaginary_2 = 0.0;
 
     const ComplexNumber complex_number_1(real_1, imaginary_1);
     const ComplexNumber complex_number_2(real_2, imaginary_2);
 
-    const double real_true_solution = 160.5;
-    const double imaginary_true_solution = 25.4;
+    const double true_real =
+        complex_number_1.getRe() + complex_number_2.getRe();
 
-    const ComplexNumber complex_numbers_true_sub(
-         real_true_solution, imaginary_true_solution);
+    const double true_imaginary = 0.0;
 
-    ASSERT_EQ(complex_number_1 - complex_number_2,
-        complex_numbers_true_sub);
+    const ComplexNumber true_complex_number(true_real, true_imaginary);
+
+    ASSERT_EQ(complex_number_1 + complex_number_2, true_complex_number);
 }
 
-TEST(Tkachev_Alexey_ComplexNumberTest, division) {
-    const double real_1 = 25.25;
-    const double imaginary_1 = 45.45;
+TEST(Tkachev_Alexey_ComplexNumberTest, multiplication_with_im_zero) {
+    const double real_1 = 25.0;
+    const double imaginary_1 = 0.0;
 
-    const double real_2 = 100.1;
-    const double imaginary_2 = 50.5;
+    const double real_2 = 30.1;
+    const double imaginary_2 = 0.0;
 
     const ComplexNumber complex_number_1(real_1, imaginary_1);
     const ComplexNumber complex_number_2(real_2, imaginary_2);
 
-    const double real_true_solution = 0.3836635041757291;
-    const double imaginary_true_solution = 0.2604894409503065;
+    const double true_real =
+        complex_number_1.getRe() * complex_number_2.getRe();
 
-    const ComplexNumber complex_numbers_true_div(
-         real_true_solution, imaginary_true_solution);
+    const double true_imaginary = 0.0;
 
-    ASSERT_EQ(complex_number_1 / complex_number_2,
-        complex_numbers_true_div);
+    const ComplexNumber true_complex_number(true_real, true_imaginary);
+
+    ASSERT_EQ(complex_number_1 * complex_number_2, true_complex_number);
 }
 
-TEST(Tkachev_Alexey_ComplexNumberTest, multiplying) {
+TEST(Tkachev_Alexey_ComplexNumberTest, division_zero_by_not_a_zero) {
     const double real_1 = 0.0;
-    const double imaginary_1 = 10.105;
+    const double imaginary_1 = 0.0;
 
-    const double real_2 = -10.105;
-    const double imaginary_2 = 35.1;
+    const double real_2 = 30.1;
+    const double imaginary_2 = 50.1;
 
     const ComplexNumber complex_number_1(real_1, imaginary_1);
     const ComplexNumber complex_number_2(real_2, imaginary_2);
 
-    const double real_true_solution = -354.68550000000005;
-    const double imaginary_true_solution = -102.11102500000001;
+    const double true_real = 0.0;
+    const double true_imaginary = 0.0;
 
-    const ComplexNumber complex_numbers_true_mul(
-         real_true_solution, imaginary_true_solution);
+    const ComplexNumber true_complex_number(true_real, true_imaginary);
 
-    ASSERT_EQ(complex_number_1 * complex_number_2,
-        complex_numbers_true_mul);
+    ASSERT_EQ(complex_number_1 / complex_number_2, true_complex_number);
 }
 
 TEST(Tkachev_Alexey_ComplexNumberTest, division_by_real_one) {
