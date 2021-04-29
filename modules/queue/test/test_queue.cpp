17f9225508bf6c55e63e5c6eaf796ba4fdaa725a
gitlng <demousegun@gmail.com>
2021-04-29 17:20:24 +0300

    test

--- modules/queue/test/test_queue.cpp
+++ modules/queue/test/test_queue.cpp
@@ -331,32 +331,6 @@ TEST(queue_test, not_empty_equals_not_empty) {
     testing_queue_2->append(value_3);
     testing_queue_2->append(value_4);
 
-    *testing_queue_1 = *testing_queue_2;
-
-    const std::string str_queue_1 = testing_queue_1->toStringData();
-    const std::string str_queue_2 = testing_queue_2->toStringData();
-
-    ASSERT_EQ(true, str_queue_1 == str_queue_2);
-}
-
-TEST(queue_test, not_empty_equals_not_empty) {
-    Queue *testing_queue_1 = new Queue();
-    Queue *testing_queue_2 = new Queue();
-    const double value_1 = 2.5;
-    const double value_2 = 3.5;
-    const double value_3 = 35.1;
-    const double value_4 = 29.99;
-
-    testing_queue_1->append(value_1);
-    testing_queue_1->append(value_2);
-
-    testing_queue_2->append(value_3);
-    testing_queue_2->append(value_4);
-
-    *testing_queue_1 = *testing_queue_2;
-
-    const std::string str_queue_1 = testing_queue_1->toStringData();
-    const std::string str_queue_2 = testing_queue_2->toStringData();
-
-    ASSERT_EQ(true, str_queue_1 == str_queue_2);
+    
+    ASSERT_NO_THROW(*testing_queue_1 = *testing_queue_2;);
 }
